357c088750439aa26a2ece6bb6c957b4fe0fee67
Leonidig <leonid200955@gmail.com>
2024-11-02 17:25:36 +0200

    done with headers

--- frontend/routes/default.py
+++ frontend/routes/default.py
@@ -1,12 +1,25 @@
 from flask import (render_template,
-                   request)
-from requests import post
+                   request,
+                   redirect,
+                   url_for)
+from requests import get, post
 
 from .. import app
 
-# @auth_app.get("/items/")
-# @auth_app.get("/users/me")
-# @auth_app.post("/token")
+
+
+@app.get("/items/<token>")
+def get_items(token: str):
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+    response = get(
+        "http://127.0.0.1:8000/items",
+        headers=headers,
+    )
+    if response.ok:
+        return response.json()
+
 
 @app.get("/login")
 def login():
@@ -25,5 +38,9 @@ def login_post():
     response = post("http://127.0.0.1:8000/token", data=data)
     # if response.ok:
     if response.status_code == 200:
-        return response.json()
+        return redirect(url_for(
+            get_items.__name__,
+            token =  response.json().get("access_token")
+            )
+        )
     return response.json().get("detail")
